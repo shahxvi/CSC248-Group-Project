a7763f09691d4f7583088bb56a3bda5a5d4e9b92
Zuqarey <zufarabqary1041@gmail.com>
2026-07-01 00:07:44 +0800

    create file handler class to hold a data form input file

---
+++ FileHandler.java
@@ -0,0 +1,9 @@
+
+import java.io.File;
+import java.applet.FileNotFoundException;
+import java.io.printWriter;
+import java.util.Scanner;
+
+public class FileHandler {
+    public abstract class FileHandler<>
+}
